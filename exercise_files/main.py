5223c609d9abe101504dbb0df7d8dbf689199274
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2022-11-26 08:21:30 +0000

    Update main.py

---
+++ exercise_files/main.py
@@ -0,0 +1,20 @@
+import sys
+
+from PyQt6.QtGui import QGuiApplication
+from PyQt6.QtQml import QQmlApplicationEngine
+from PyQt6.QtQuick import QQuickWindow
+
+from func import Backend
+
+
+QQuickWindow.setSceneGraphBackend('software')
+
+app = QGuiApplication(sys.argv)
+
+engine = QQmlApplicationEngine()
+back_end = Backend()
+engine.load('./UI/main.qml')
+engine.rootObjects()[0].setProperty('backend', back_end)
+engine.quit.connect(app.quit)
+
+sys.exit(app.exec())
