32d41697ce604bd03e62754a7fe662bb5943cf10
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2025-03-26 16:35:35 +0000

    Update complete.py

--- mds/complete.py
+++ mds/complete.py
@@ -31,19 +31,25 @@ def code_tag(md_path: str) -> str:
     with open(os.path.join(ART_EDUCATIVE, md_path), 'r') as md_file:
         md_conts = md_file.read()
         code_tags = re.findall(r"`code[0-9]?.txt`", md_conts)
+
+    md_path_r = md_path.replace('.md', '')
+    com_md_path = '/'.join([COMPLETE, md_path])
+    os.makedirs(com_md_path)
+
     for tag in code_tags:
         # find file in educative
-        md_path_r = md_path.replace('.md', '')
         r_tag = tag.replace("`", "")
         code_path = '/'.join([EDUCATIVE, md_path_r, r_tag])
-        
+
         with open(code_path, 'r') as code_file:
             code_conts = code_file.read()
 
+        md_conts = md_conts.replace(tag, "`"+code_conts+"`")
+
         # replace file
-        with open('/'.join([COMPLETE, md_path_r, r_tag])+".md", 'w') as t_wri:
-            t_wri.write(code_conts)
-    return replaced_content
+        with open(com_md_path, 'w') as t_wri:
+            t_wri.write(md_conts)
+    return ''
 
 
 #art_edus = scan_art_edu()
