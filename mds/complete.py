3608e065b2f97de957384ec78ae696012f3ae6a1
Amoh Gyebi Ampofo <amohgyebigodwin@gmail.com>
2025-03-26 15:20:29 +0000

    Update complete.py

--- mds/complete.py
+++ mds/complete.py
@@ -1,1 +1,19 @@
 import os
+
+
+ART_EDUCATIVE = './educative'
+COMPLETE = './complete'
+EDUCATIVE = "C:/Users/amohgyebi/Documents/GitHub/educative"
+
+# scan contents of art_educative folder
+# open each md
+# find those that contain code and replace it with the
+# contents of code from educative
+
+
+def scan_art_edu() -> list:
+    conts = os.path.listdir(ART_EDUCATIVE)
+    return conts
+
+
+art_edus = scan_art_edu()
